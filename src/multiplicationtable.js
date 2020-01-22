fa98ed3522356941823b059e41130e7f0dcbe9cd
zzanguna <yajung0203@naver.com>
2020-01-22 18:09:02 +0900

    숙제 value 정답! 나오도록 하기

--- src/multiplicationtable.js
+++ src/multiplicationtable.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';//react 코드를 웹에다가 붙여주는 코드
+import './index.css';
 
+//숙제 16 정답! 뜨도록 하기
 class GuGuDan extends React.Component{
     constructor(props) {
         super(props);
@@ -8,6 +10,7 @@ class GuGuDan extends React.Component{
             first : Math.ceil(Math.random() * 9),
             second : Math.ceil(Math.random() * 9),
             value:'',
+            resultval : '',
             result: ''
 
         }
@@ -15,10 +18,12 @@ class GuGuDan extends React.Component{
     //메소드를 만들어 준다.
     onSubmit = (e) => {
         e.preventDefault();
-        if(parseInt(this.state.vlaue) === this.state.first * this.state.second){
+        var resultevale = this.state.value;
+        if(parseInt(this.state.value) === this.state.first * this.state.second){
             this.setState({
                 first : Math.ceil(Math.random() * 9),
                 second : Math.ceil(Math.random() * 9),
+                resultval : resultevale,
                 value:'',
                 result: '정답'
             })
@@ -37,14 +42,12 @@ class GuGuDan extends React.Component{
             <div>
                 <div>{this.state.first}곱하기{this.state.second}는?</div>
                 <form onSubmit={this.onSubmit}>
-                    {/*<input type="number" value={this.state.value} onChange={(e) => this.setState({value: e.target.value})}/>
-                    메소드를 만들어서 this.onChange로 불러오는 것이 좋다*/}
                     <input type="number" value={this.state.value} onChange={this.onChange}/>
                     {/*input.onchage = function(e){console.log(e.target.value)}*/}
                     <button>입력!</button>
                 </form>
                 <div>
-                    {this.state.result}
+                    {this.state.resultval} {this.state.result}
                 </div>
             </div>
         )
