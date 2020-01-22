b12b03d3b93e6744f5866cc43daa0c49eee898aa
zzanguna <yajung0203@naver.com>
2020-01-22 17:08:05 +0900

    jsx와 바벨

--- src/index.js
+++ src/index.js
@@ -2,25 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';//react 코드를 웹에다가 붙여주는 코드
 import './index.css';
 
+
 const e = React.createElement;
 class LikeButton extends React.Component{
-    constructor(props) {//리액트에서는 기본적으로 쓰는 부분
+    constructor(props) {
         super(props);
-        //없는거나 마찬가지이다.
+
         this.state = {
             liked : false,
 
         };
     }
     render() {
-        return <button type="submit" onClick={()=>{this.setState({liked:true})}}>Liked</button>
-        //가운데는 속성을 넣어주는 것이다.
-        //상태는 바뀔 수 잇는부분 바뀌는 부분
-        //<button onclick="() => {console.log('clicked')" type="submit">Like<button> 이 태그를 만들겠다.
+        //javascript에서 html 태그를 쓰는것는 말이 안된다.
+        //밑에 태그 쓰는 것을 지원 하지 않기 때문에 babel.js 를 사용한다
+        return <button type="submit" onClick={()=>{this.setState({liked:true})}}>
+            {this.state.liked === true ? "Like" : "Liked"}
+        </button>
+        // jsx : (js + xml)
     }
 
 
 }
-
-ReactDOM.render(<LikeButton />,document.querySelector('#root'));
-//react는 위에서부터 아래로 불러 온다
+//4개가 생기겼다.
+//기존 html jquery에서 수정을 하려면
+// <button>종하요<button/><button>종하요<button/><button>종하요<button/><button>종하요<button/><button>종하요<button/>
+//많은 이름 하나라도 변경하려면 하면 하나하나 직접 쳐야하지만 컴포넌트만 수정하면 끝난다.
+//재 사용성
+//실재로는 한줄짜리 컴포넌트태그지만 실제로는 수십줄의 코드인다.
+ReactDOM.render(<div><LikeButton /><LikeButton /><LikeButton /><LikeButton /><LikeButton /></div>,document.querySelector('#root'));
