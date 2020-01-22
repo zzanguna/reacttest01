bc52c385f57bb3f5d590191b65824e54603dc835
zzanguna <yajung0203@naver.com>
2020-01-22 18:38:55 +0900

    함수형 setState

--- src/multiplicationtable.js
+++ src/multiplicationtable.js
@@ -2,21 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';//react 코드를 웹에다가 붙여주는 코드
 import './index.css';
 
-//숙제 16 정답! 뜨도록 하기
 class GuGuDan extends React.Component{
-
-    /*constructor(props) {
-        super(props);
-        this.state = {
-            first : Math.ceil(Math.random() * 9),
-            second : Math.ceil(Math.random() * 9),
-            value:'',
-            resultval : '',
-            result: ''
-
-        }
-    }*/
-    //실무에서는 constructor 안쓰고 그냥 state 선언해준다.
     state = {
         first : Math.ceil(Math.random() * 9),
         second : Math.ceil(Math.random() * 9),
@@ -25,36 +11,41 @@ class GuGuDan extends React.Component{
         result: ''
 
     }
-    //메소드를 만들어 준다.
-    //클래스 메소는 무조건 화살표 함수 써야한다.
-    //function을 쓴다는 this가 달라져서 안된다.
+
     onSubmit = (e) => {
         e.preventDefault();
-        var resultevale = this.state.value;
         if(parseInt(this.state.value) === this.state.first * this.state.second){
-            this.setState({
+            //숙제 선생님 답
+            /*this.setState({
+                result: '정답 : ' + this.state.value,
                 first : Math.ceil(Math.random() * 9),
                 second : Math.ceil(Math.random() * 9),
-                resultval : resultevale,
                 value:'',
-                result: '정답'
+            })*/
+            //setState를 함수화 하는 방법
+            this.setState((prevSate)=>{//prevSate : 예전 상태값
+                return {
+                    result: '정답은 : ' + prevSate.value,
+                    first : Math.ceil(Math.random() * 9),
+                    second : Math.ceil(Math.random() * 9),
+                    value:'',
+                }
             })
+
+
         }
         else {
             this.setState({
                 value:'',
-                result: '땡'
+                result: '땡',
             })
         }
     };
     onChange = (e) => this.setState({value: e.target.value});
-    //div가 하나 더 있다고 문제가 없지만
-    //쓸데 없다고 생각하는 div를 지우고 싶다면 <></> 또는 <React.Fragment>를 해야한다.
-    //() : 그룹 연산자 우선 순위 높이는 것 말고는 아무 의미 없다.
-    //컨텐츠
+
     render() {
         return (
-            <>
+            <div>
                 <div>{this.state.first}곱하기{this.state.second}는?</div>
                 <form onSubmit={this.onSubmit}>
                     <input type="number" value={this.state.value} onChange={this.onChange}/>
@@ -62,9 +53,9 @@ class GuGuDan extends React.Component{
                     <button>입력!</button>
                 </form>
                 <div>
-                    {this.state.resultval} {this.state.result}
+                    {this.state.result}
                 </div>
-            </>
+            </div>
         )
     }
 
